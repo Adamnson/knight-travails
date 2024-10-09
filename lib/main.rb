7dff346bceab7a7fd574a127fc8e36e7affd6d2b
Adamnson-0dinson <theawakeavatar@gmail.com>
2024-10-09 17:07:50 +0530

    (feat!) removed class Chess and refactoring code,test

--- lib/main.rb
+++ lib/main.rb
@@ -3,63 +3,62 @@ require "pry-byebug"
 # class chess contains the following methods:
 #  calc_next_pos: calculates the possible next moves from a given position
 #  calc_dist    : calculates euclidean distance between two points
-class Chess
-  def calc_next_pos(loc)
-    x1 = [loc[0] + 1, loc[0] - 1]
-    x2 = [loc[0] + 2, loc[0] - 2]
-    y1 = [loc[1] + 1, loc[1] - 1]
-    y2 = [loc[1] + 2, loc[1] - 2]
-    pos = []
-    pos.append(x1.product(y2).filter do |xy|
-      (0...8).include?(xy[0]) and (0...8).include?(xy[1])
-    end).flatten.each_slice(2).to_a
-    pos.append(x2.product(y1).filter do |xy|
-      (0...8).include?(xy[0]) and (0...8).include?(xy[1])
-    end).flatten.each_slice(2).to_a
-  end
 
-  def calc_dist(loc1, loc2)
-    Math.sqrt((loc1[0] - loc2[0]).pow(2) + (loc1[1] - loc2[1]).pow(2))
-  end
+def calc_next_pos(loc)
+  x1 = [loc[0] + 1, loc[0] - 1]
+  x2 = [loc[0] + 2, loc[0] - 2]
+  y1 = [loc[1] + 1, loc[1] - 1]
+  y2 = [loc[1] + 2, loc[1] - 2]
+  pos = []
+  pos.append(x1.product(y2).filter do |xy|
+    (0...8).include?(xy[0]) and (0...8).include?(xy[1])
+  end).flatten.each_slice(2).to_a
+  pos.append(x2.product(y1).filter do |xy|
+    (0...8).include?(xy[0]) and (0...8).include?(xy[1])
+  end).flatten.each_slice(2).to_a
 end
 
-n1 = Chess.new
+def calc_dist(loc1, loc2)
+  Math.sqrt((loc1[0] - loc2[0]).pow(2) + (loc1[1] - loc2[1]).pow(2))
+end
+
 start = [1, 2]
 target = [6, 5]
 
-dist_hash = {}
-dist_to_target = n1.calc_dist(start, target)
-p "iteration before start #{dist_to_target}"
-seq = {}
-seq_id = 1
-
 # binding.pry
-
-while dist_to_target.positive? && seq_id < 10
-
-  next_pos = n1.calc_next_pos(start)
+def knight_moves(start, target)
   dist_hash = {}
 
-  next_pos.each_with_index do |pt, idx|
-    dist_hash[idx] = n1.calc_dist(pt, target)
-  end
+  dist_to_target = calc_dist(start, target)
+  p "iteration before start #{dist_to_target}"
+  seq = {}
+  seq_id = 1
+  while dist_to_target.positive? && seq_id < 10
 
-  dist_to_target = dist_hash.values.min
-  key_min_dist = dist_hash.key(dist_to_target)
+    next_pos = calc_next_pos(start)
+    dist_hash = {}
 
-  p "is it included? #{seq.values.include?(next_pos[key_min_dist])}"
+    next_pos.each_with_index do |pt, idx|
+      dist_hash[idx] = calc_dist(pt, target)
+    end
 
-  if seq.values.include?(next_pos[key_min_dist])
-    dist_to_target = dist_hash.values.uniq.min(2)[1]
+    dist_to_target = dist_hash.values.min
     key_min_dist = dist_hash.key(dist_to_target)
+
+    p "is it included? #{seq.values.include?(next_pos[key_min_dist])}"
+
+    if seq.values.include?(next_pos[key_min_dist])
+      dist_to_target = dist_hash.values.uniq.min(2)[1]
+      key_min_dist = dist_hash.key(dist_to_target)
+    end
+
+    p [next_pos[key_min_dist], dist_hash[key_min_dist]]
+    seq[seq_id] = next_pos[key_min_dist]
+    start = next_pos[key_min_dist]
+    seq_id += 1
+    puts "at the end of #{seq_id}, seq looks like #{seq}"
   end
-
-  p [next_pos[key_min_dist], dist_hash[key_min_dist]]
-  seq[seq_id] = next_pos[key_min_dist]
-  start = next_pos[key_min_dist]
-  seq_id += 1
-  puts "at the end of #{seq_id}, seq looks like #{seq}"
-
+  seq
 end
-# seq.append(target)
-p seq
+
+p knight_moves(start, target)
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,30 +1,26 @@
 require "./lib/main"
 
-describe Chess do
-  context "#calc_next_pos" do
-    n1 = Chess.new
-    it "calculates 8 valid positions when available" do
-      pos = n1.calc_next_pos([3, 3])
-      exp_pos = [[4, 5], [4, 1], [2, 5], [2, 1], [5, 4], [5, 2], [1, 4], [1, 2]]
-      expect(pos).to eql(exp_pos)
-      expect(pos.size).to eql(8)
-    end
-
-    it "calculates 2 valid positions for corner nodes" do
-      pos = n1.calc_next_pos([0, 0])
-      exp_pos = [[1, 2], [2, 1]]
-      expect(pos).to eql(exp_pos)
-      expect(pos.size).to eql(2)
-    end
+context "#calc_next_pos" do
+  it "calculates 8 valid positions when available" do
+    pos = calc_next_pos([3, 3])
+    exp_pos = [[4, 5], [4, 1], [2, 5], [2, 1], [5, 4], [5, 2], [1, 4], [1, 2]]
+    expect(pos).to eql(exp_pos)
+    expect(pos.size).to eql(8)
   end
 
-  context "#calc_dist" do
-    n1 = Chess.new
-    it "returns the euclidean distance between two points" do
-      expect(n1.calc_dist([0, 0], [1, 1])).to eql(Math.sqrt(2))
-      expect(n1.calc_dist([2, 2], [1, 1])).to eql(Math.sqrt(2))
-      expect(n1.calc_dist([2, 2], [0, 0])).to eql(Math.sqrt(8))
-      expect(n1.calc_dist([0, 0], [0, 0])).to eql(Math.sqrt(0))
-    end
+  it "calculates 2 valid positions for corner nodes" do
+    pos = calc_next_pos([0, 0])
+    exp_pos = [[1, 2], [2, 1]]
+    expect(pos).to eql(exp_pos)
+    expect(pos.size).to eql(2)
+  end
+end
+
+context "#calc_dist" do
+  it "returns the euclidean distance between two points" do
+    expect(calc_dist([0, 0], [1, 1])).to eql(Math.sqrt(2))
+    expect(calc_dist([2, 2], [1, 1])).to eql(Math.sqrt(2))
+    expect(calc_dist([2, 2], [0, 0])).to eql(Math.sqrt(8))
+    expect(calc_dist([0, 0], [0, 0])).to eql(Math.sqrt(0))
   end
 end
